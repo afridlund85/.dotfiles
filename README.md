c6441a0e99a63c9e04449f120995ec4b0948cb74
Andreas Fridlund <afridlund85@gmail.com>
2022-06-27 22:41:01 +0200

    fixed ubuntu install and docker stuff for dev

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # .dotfiles
+
+## Install
+
+### Ubuntu
+
+```
+sudo apt update -qq && \
+sudo apt install -qq -y git && \
+git clone https://github.com/afridlund85/.dotfiles.git ~/.dotfiles && \
+cd ~/.dotfiles && \
+sudo ./install_ubuntu.sh
+```
+
+Start nvim and run `:PackerSync`
+
+or
+
+Open plugins and save it (ctrl + w)
+
+```
+nvim nvim/.config/nvim/lua/afridlund85/plugins.lua
+```
+
+## Dev
+
+### Ubuntu
+
+```
+docker build -t dotfiles_ubuntu -f ./Dockerfile-ubuntu .
+docker run --rm -it --name dotfiles_ubuntu_dev -u 1001:1001 dotfiles_ubuntu /bin/bash
+```
--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
+apt -qq update
+apt -qq -y install curl wget git
+
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash
-add-apt-repository ppa:neovim-ppa/stable
-apt update
-apt install -y software-properties-common neovim stow \
+apt -qq update
+apt -qq -y install build-essential software-properties-common g++ stow \
     nodejs \
     php8.1-{bcmath,xml,mysql,zip,intl,ldap,gd,cli,bz2,curl,mbstring,pgsql,opcache,soap,cgi}
 
+curl -fsSLO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.deb
+apt install -qq -y ./nvim-linux64.deb
+rm nvim-linux64.deb
+
 curl -OL https://go.dev/dl/go1.17.7.linux-amd64.tar.gz
-tar -C /usr/local -xvf go1.17.7.linux-amd64.tar.gz
+tar -C /usr/local -xf go1.17.7.linux-amd64.tar.gz
 rm go1.17.7.linux-amd64.tar.gz 
 
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-mkdir -p ~/.local/bin
-mv composer.phar ~/local/bin/composer
+wget https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer -O - -q | php -- --quiet
+mkdir -p ~/.local/bin && mv composer.phar ~/.local/bin/composer
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+~/.fzf/install --all
 
 stow git
 stow nvim
@@ -49,7 +51,5 @@ curl -fLo ~/.local/share/fonts/"Droid Sans Mono Nerd Font Complete.otf" \
 https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 
 # Nvim
-sh -c 'curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-nvim --headless +PlugInstall +qall
-
+# Currently not working properly: https://github.com/wbthomason/packer.nvim/issues/897
+# nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
