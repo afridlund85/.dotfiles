44c3b5b3b40d2daf8033a5fc434d875674a1f8ce
Andreas Fridlund <andreas.fridlund@fortnox.se>
2023-05-26 08:42:43 +0200

    php dap wip

--- nvim/.config/nvim/lua/afridlund85/dap/php.lua
+++ nvim/.config/nvim/lua/afridlund85/dap/php.lua
@@ -3,16 +3,29 @@ if not dap_status_ok then
 	return
 end
 
+local mason_registry = require("mason-registry")
+
+local codelldb = mason_registry.get_package("php-debug-adapter")
+local extension_path = codelldb:get_install_path() .. "/extension/"
+local php_debug_path = extension_path .. "out/phpDebug.js"
+
 dap.adapters.php = {
 	type = "executable",
-	command = "/home/andreas/.local/share/nvim/mason/bin/php-debug-adapter",
+	command = "node",
+	args = { php_debug_path }
+	-- command = "/home/andreas/.local/share/nvim/mason/bin/php-debug-adapter",
 }
 
 dap.configurations.php = {
 	{
 		type = "php",
 		request = "launch",
-		name = "Listen for Xdebug",
-		port = "9003"
+		name = "Xdebug PHP",
+		port = "9003",
+		pathMapping= {
+			["/storage/vol3/fortnox-andfri"] = "/home/andreas/dev/fortnox"
+		},
+		serverSourceRoot = "/storage/vol3/fortnox-andfri",
+		localSourceRoot = "/home/andreas/dev/fortnox"
 	}
 }
