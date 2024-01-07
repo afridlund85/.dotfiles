07b786bd0e4985911c35dfc85b999d9935090002
Andreas Fridlund <andreas.fridlund@fortnox.se>
2024-01-07 20:36:15 +0100

    php debug

--- nvim/.config/nvim/lua/afridlund85/dap/php.lua
+++ nvim/.config/nvim/lua/afridlund85/dap/php.lua
@@ -17,22 +17,31 @@ dap.adapters.php = {
 }
 
 dap.configurations.php = {
-	-- {
-	-- 	type = "php",
-	-- 	request = "launch",
-	-- 	name = "Xdebug PHP",
-	-- 	port = 9003,
-	-- 	-- pathMapping= {
-	-- 	-- 	["/storage/vol3/fortnox-andfri"] = "/home/andreas/dev/fortnox"
-	-- 	-- },
-	-- },
 	{
 		type = "php",
 		request = "launch",
-		name = "Remote F3",
+		name = "Xdebug PHP",
+		port = 9003,
+		-- pathMapping= {
+		-- 	["/storage/vol3/fortnox-andfri"] = "/home/andreas/dev/fortnox"
+		-- },
+	},
+	{
+		type = "php",
+		request = "launch",
+		name = "Remote F3 UI",
 		port = 9000,
 		log = true,
 		serverSourceRoot = "/storage/vol3/fortnox-andfri/ui",
 		localSourceRoot = "/home/andreas/dev/fortnox/ui"
+	},
+	{
+		type = "php",
+		request = "launch",
+		name = "Remote F3 BACKEND",
+		port = 9000,
+		log = true,
+		serverSourceRoot = "/storage/vol3/fortnox-andfri/webapp",
+		localSourceRoot = "/home/andreas/dev/fortnox/webapp"
 	}
 }
